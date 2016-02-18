cc80f61798d962afbf4d66fc163cd093891caffd
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-02-17 17:11:19 -0800

    canvas

--- canvas.js
+++ canvas.js
@@ -2,7 +2,7 @@
  * Created by sunmy on 16/1/29.
  */
 
-/** 路径绘制 **/
+/** 路径 **/
 /** path **/
 context.beginPath(); // 未开启新路径 路径首尾相接
 context.closePath();
@@ -22,6 +22,8 @@ context.bezierCurveTo(cX1, cY1, cX2, cY2, endX, endY);
 
 
 
+
+/** 画布 **/
 /** clip **/
 // 存储画布未被裁切时状态
 context.save();
@@ -43,53 +45,30 @@ context.beginPath();
 context.arc(250, 250, 100, 0, 2 * Math.PI);
 context.stroke();
 
+/** transform **/
+context.rotate(angle);
+context.translate(x, y);
+context.scale(num, num);
+context.transform(a, b, c, d, e, f);
+context.setTransform(a, b, c, d, e, f);
+
+/** imageData **/
+context.getImageData(x, y, w, h);
+context.putImageData(data, x, y, dirtyX, dirtyY, dirtyW, dirtyH); // 不受全局属性影响
+context.createImageData(w, h);
+context.drawImage(imageObj, sX, sY, sW, sH, dX, dY, dW, dH); // 须在图片加载完毕后 执行 受全局属性影响
 
 
 
 
-
-
-
-
-
-/** gradient **/
-/** 注意渐变圆位置关系 **/
+/** 填充 **/
+/** gradient
+ * 注意渐变圆位置关系
+ */
 var gradient = context.createLinearGradient(startX, startY, endX, endY);
 gradient.addColorStop(point, color);
-
 var gradient = context.createRadialGradient(startX, startY, startR, endX, endY, endR);
 gradient.addColorStop(point, color);
-
-
-
-/** shadow
- * .shadowColor
- * .shadowOffsetX
- * .shadowOffsetY
- * .shadowBlur
- */
-
-
-
-
-/**
- * .globalAlpha
- * .globalCompositeOperation 图像合成模式
- */
-
-
-/**
- * .lineWidth
- * .lineJoin
- * .lineCap
- * .miterLimit 斜接线
- *
- * .font
- * .textAlign
- * .textBaseline
- */
-
-
 
 /** 非零环绕规则
  * 区域校验线段 通过路径 方向矢量和 为 0 不填充
@@ -102,30 +81,57 @@ context.fill();
 
 
 
-
-
-
-
+/** 绘制 **/
 /** rectangle **/
-context.clearRect(x, y, w, h);
 context.strokeRect(x, y, w, h);
 context.fillRect(x, y, w, h);
+context.clearRect(x, y, w, h);
 /** text **/
 context.strokeText(text, x, y, maxWidth); // maxWidth 压缩 非 隐藏
 context.fillText(text, x, y, maxWidth);
 context.measureText(text); // 返回文本占位宽度
 
 
-/** coordinate **/
-context.rotate(angle);
-context.translate(x, y);
-context.scale(num, num);
-context.transform(a, b, c, d, e, f);
-context.setTransform(a, b, c, d, e, f);
 
 
-/** imageData **/
-context.getImageData(x, y, w, h);
-context.putImageData(data, x, y, dirtyX, dirtyY, dirtyW, dirtyH); // 不受全局属性影响
-context.createImageData(w, h);
-context.drawImage(imageObj, sX, sY, sW, sH, dX, dY, dW, dH); // 须在图片加载完毕后 执行 受全局属性影响
+/** 属性 **/
+/** 全局
+ * .globalAlpha
+ * .globalCompositeOperation 图像合成模式
+ */
+
+/** 阴影
+ * .shadowColor
+ * .shadowOffsetX
+ * .shadowOffsetY
+ * .shadowBlur
+ */
+
+/** 线
+ * .lineWidth
+ * .lineJoin
+ * .lineCap
+ * .miterLimit 斜接线
+ */
+
+/** 文本
+ * .font
+ * .textAlign
+ * .textBaseline
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

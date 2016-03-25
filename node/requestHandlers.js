305d12e5a0649d9b92f0b3633c28c4b1dc4e99af
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-03-25 12:16:56 -0700

    nodejs note

--- node/requestHandlers.js
+++ node/requestHandlers.js
@@ -3,14 +3,10 @@
  */
 
 function start(response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Request handler 'start' was called");
-    response.end();
+
 }
 function upload(response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Request handler 'upload' was called");
-    response.end();
+
 }
 
 exports.start = start;

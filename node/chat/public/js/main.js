fd18af79692423d652ad2f6d2316ecbe385dab1c
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-07-15 12:13:57 +0800

    chat框架

--- node/chat/public/js/main.js
+++ node/chat/public/js/main.js
@@ -1,3 +1,11 @@
 /**
  * Created by sunmy on 16/7/14.
  */
+
+var main = {
+    do: function () {
+        var _this = this;
+    }
+};
+
+main.do();
--- node/chat/routes.js
+++ node/chat/routes.js
@@ -1,3 +1,13 @@
 /**
  * Created by sunmy on 16/7/14.
  */
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.render('index');
+});
+
+
+module.exports = router;

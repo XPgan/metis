ba88812b3f8f6c86fcc8b7f44e5ed9c22d0e6da8
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-07-29 14:04:26 +0800

    重复用户名逻辑判断

--- node/chat/modules/edit.js
+++ node/chat/modules/edit.js
@@ -7,17 +7,35 @@ var User = require('../models').User;
 var edit = {
     user: {
         info: function (req, res) {
-            User.update({id: req.cookies.user}, req.body, {}, function (err) {
+            var cur_user = req.cookies.user;
+            User.find({user_name: req.body.user_name}, {}, {}, function (err, result) {
                 if (err) {
                     res.end(JSON.stringify({
                         message: '提交失败',
                         status: 0
                     }));
                 } else {
-                    res.end(JSON.stringify({
-                        message: '提交成功',
-                        status: 1
-                    }));
+                    var judge = result.length && (result[0].id != cur_user);
+                    if (judge) {
+                        res.end(JSON.stringify({
+                            message: '该昵称已被使用',
+                            status: 0
+                        }));
+                    } else {
+                        User.update({id: cur_user}, req.body, {}, function (err) {
+                            if (err) {
+                                res.end(JSON.stringify({
+                                    message: '提交失败',
+                                    status: 0
+                                }));
+                            } else {
+                                res.end(JSON.stringify({
+                                    message: '提交成功',
+                                    status: 1
+                                }));
+                            }
+                        });
+                    }
                 }
             });
         }

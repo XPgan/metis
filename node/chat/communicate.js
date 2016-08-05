dce1edf4cffa4052d45c4b23ed5fd981cd01646c
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-08-05 11:02:05 +0800

    下线提醒逻辑

--- node/chat/communicate.js
+++ node/chat/communicate.js
@@ -18,36 +18,27 @@ var method = {
 };
 var communicate = {
 
-    interval: null,
     activers: {},
 
     do: function (io) {
         var _this = this;
         io.on('connection', function (socket) {
             _this.handleEvents(socket, io);
-            _this.setActivers(socket, true);
-            _this.checkActivers(io);
-
-            socket.on('disconnect', function () {
-                _this.setActivers(socket, false);
-            });
+            _this.checkActivers(socket, io);
         });
     },
     handleEvents: function (socket, io) {
-        var _this = this;
         socket.on('online', function (data) {
             io.sockets.emit('online', {
                 id: data.id,
                 nickname: data.nickname
             });
-            _this.setActivers(socket, true);
         });
         socket.on('offline', function (data) {
             io.sockets.emit('offline', {
                 id: data.id,
                 nickname: data.nickname
             });
-            delete _this.activers[data.id];
         });
         socket.on('message', function (data) {
             io.sockets.emit('message', {
@@ -59,31 +50,29 @@ var communicate = {
             });
         });
     },
-    checkActivers: function (io) {
-        var _this = this;
-        _this.interval && clearInterval(_this.interval);
-        _this.interval = setInterval(function () {
-            for (var key in _this.activers) {
-                var _loop = _this.activers[key];
-                if (!_loop.status) {
-                    io.sockets.emit('offline', {
-                        id: key,
-                        nickname: _loop.nickname
-                    });
-                    delete _this.activers[key];
-                }
-            }
-        }, 5000);
-    },
-    setActivers: function (socket, status) {
+    checkActivers: function (socket, io) {
         var _this = this;
         var cur_user = method.getCookie(socket, 'user');
-        if (cur_user) {
-            _this.activers[cur_user.id] = {
-                status: status,
-                nickname: cur_user.nickname
+
+        (cur_user) && (_this.activers[cur_user.id] = true);
+
+        socket.on('disconnect', function () {
+            if (cur_user) {
+                _this.activers[cur_user.id] = false;
+
+                var t = setTimeout(function () {
+                    var status = _this.activers[cur_user.id];
+                    if (!status) {
+                        io.sockets.emit('offline', {
+                            id: cur_user.id,
+                            nickname: cur_user.nickname
+                        });
+
+                        delete _this.activers[cur_user.id];
+                    }
+                }, 2000);
             }
-        }
+        });
     }
 };
 

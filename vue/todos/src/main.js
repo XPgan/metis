6939c7cef291947be30980838bb8d4bab616f7aa
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-09-08 18:02:30 +0800

    todos 目录结构

--- vue/todos/src/main.js
+++ vue/todos/src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
+import {sync} from 'vuex-router-sync'
+import store from './vuex/store'
+import router from './router'
 import App from './App'
 
-/* eslint-disable no-new */
-new Vue({
-    el: 'body',
-    components: {App}
-})
+sync(store, router)
+
+router.start(App, '#app')

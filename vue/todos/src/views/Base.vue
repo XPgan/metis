590982b89b9336dd5872ea7b5621127d65ee15b7
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-09-09 15:24:43 +0800

    input ui

--- vue/todos/src/views/Base.vue
+++ vue/todos/src/views/Base.vue
@@ -1,8 +1,8 @@
 <template>
-    <section class="l-mn">
+    <section class="l-mn c-center">
         <div class="m-input">
-            <input type="text" placeholder="What needs to be done?" />
-            <button>Add</button>
+            <input type="text" placeholder="What needs to be done" />
+            <a href="javascript:;">Add</a>
         </div>
         <div class="m-lst">
             <router-view></router-view>
@@ -22,3 +22,34 @@
         name: 'Base'
     }
 </script>
+
+<style lang="less" scoped>
+    .l-mn {
+        position: fixed;
+        width: 500px;
+
+        .m-input {
+            position: relative;
+            padding-right: 90px;
+
+            input, a {
+                display: block;
+                font-size: 22px;
+                line-height: 44px;
+            }
+            input {
+                width: 100%;
+                color: #666;
+                border-bottom: 1px solid #42b983;
+            }
+            a {
+                position: absolute;
+                bottom: 0;
+                right: 0;
+                color: #fff;
+                background-color: #42b983;
+                padding: 0 20px;
+            }
+        }
+    }
+</style>

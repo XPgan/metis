f018c4a049cc3811482ae9fdc3e8b9e2c878e865
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-09-21 17:06:17 +0800

    zone node结构

---
+++ vue/zone/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+module.exports = app;

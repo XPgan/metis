e3a877267de92b35f5c19919f5f8cf65445b4545
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-09-22 15:07:12 +0800

    试验接口

--- vue/zone/routes.js
+++ vue/zone/routes.js
@@ -1,3 +1,15 @@
 /**
  * Created by sunmy on 16/9/22.
  */
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.end(JSON.stringify({
+        message: 'message',
+        status: 1
+    }));
+});
+
+module.exports = router;

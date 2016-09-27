1f230c25694498c62f504780b1917b92f8c8d398
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-09-27 12:22:35 +0800

    编辑个人信息

--- vue/zone/fe/src/views/Profile.vue
+++ vue/zone/fe/src/views/Profile.vue
@@ -5,6 +5,23 @@
         <articles :articles="articles"></articles>
         <div class="zone-btn"><a href="javascript:;" class="zone-more">查看更多</a></div>
     </section>
+    <div class="c-mask">
+        <form enctype="multipart/form-data" id="form_edituser" class="zone-form c-center">
+            <div class="form-upload">
+                <img src="../assets/default_photo.png" />
+                <input type="file" id="portrait" name="portrait" />
+                <span>更改头像</span>
+            </div>
+            <div class="form-info">
+                <input type="text" id="nickname" name="nickname" placeholder="昵称" />
+                <input type="password" id="old_password" name="old_password" placeholder="原密码" />
+                <input type="password" id="password" name="password" placeholder="新密码" />
+                <input type="password" placeholder="确认密码" />
+                <input type="text" id="intro" name="intro" placeholder="一句话描述自己" />
+                <div class="zone-btn"><a href="javascript:;">提交</a><a href="javascript:;">取消</a></div>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -92,5 +109,14 @@
 </script>
 
 <style lang="less" scoped>
-
+    form.zone-form { position: fixed; }
+    form.zone-form .form-upload img, form.zone-form .form-upload input { width: 100px;height: 100px; }
+    form.zone-form .form-upload {
+        width: 100px;
+        margin: 50px auto -130px;
+    }
+    form.zone-form .form-info {
+        padding-top: 80px;
+        margin: 50px auto;
+    }
 </style>

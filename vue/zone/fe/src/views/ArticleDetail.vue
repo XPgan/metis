8c6317f4d311f11d5ed1afde5b79c837109e28d4
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-09-27 16:05:13 +0800

    编辑按钮

--- vue/zone/fe/src/views/ArticleDetail.vue
+++ vue/zone/fe/src/views/ArticleDetail.vue
@@ -1,7 +1,7 @@
 <template>
     <user-info></user-info>
     <section>
-        <h4 class="article-title">这里是标题</h4>
+        <h4 class="article-title">这里是标题<a href="javascript:;"></a></h4>
         <div class="article-content">这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文</div>
         <div class="zone-btn"><a href="javascript:;">继续阅读</a></div>
     </section>
@@ -23,6 +23,16 @@
         font-size: 18px;
         line-height: 30px;
         color: #5c2b27;
+
+        a {
+            display: inline-block;
+            vertical-align: middle;
+            width: 20px;
+            height: 30px;
+            background: url("../assets/ico_edit.png") no-repeat 50% 2px;
+            background-size: 20px auto;
+            margin-left: 10px;
+        }
     }
     .article-content {
         font-size: 16px;

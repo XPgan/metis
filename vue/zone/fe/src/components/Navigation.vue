fa8631c45cd603f1335fffad2a40532dee945e06
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-09-27 16:45:16 +0800

    登录框

--- vue/zone/fe/src/components/Navigation.vue
+++ vue/zone/fe/src/components/Navigation.vue
@@ -4,16 +4,16 @@
             <li><a v-link="{ path: '/' }">首页</a></li>
             <li><a href="https://github.com/sunmengyuan/note/tree/master/vue/zone">源码</a></li>
         </ul>
-        <div class="m-log"><a href="javascript:;">登录</a><a v-link="{ path: '/register' }">注册</a></div>
+        <div class="m-log"><a href="javascript:;" @click="toggleLogin">登录</a><a v-link="{ path: '/register' }">注册</a></div>
     </div>
-    <div class="c-mask">
+    <div class="c-mask" v-show="showLogin">
         <form enctype="multipart/form-data" id="form_login" class="zone-form">
             <div class="form-info c-center">
                 <h3>登录<a v-link="{ path: '/register' }">注册</a></h3>
                 <input type="text" id="nickname" name="nickname" placeholder="昵称" />
                 <input type="password" id="password" name="password" placeholder="密码" />
                 <span>这里是提示信息</span>
-                <div class="zone-btn"><a href="javascript:;">确定</a><a href="javascript:;">取消</a></div>
+                <div class="zone-btn"><a href="javascript:;">确定</a><a href="javascript:;" @click="toggleLogin">取消</a></div>
             </div>
         </form>
     </div>
@@ -21,7 +21,18 @@
 
 <script>
     export default {
-        name: 'Navigation'
+        name: 'Navigation',
+        data () {
+            return {
+                showLogin: 0
+            }
+        },
+        methods: {
+            toggleLogin () {
+                var status = this.showLogin
+                this.showLogin = status ? 0 : 1
+            }
+        }
     }
 </script>
 

c1147d88ac247fbdc37d8e395d88646b3ce8f95d
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-09-29 18:28:01 +0800

    vuex记录登录状态废弃

--- vue/zone/fe/src/vuex/actions.js
+++
@@ -1,8 +0,0 @@
-import * as types from './mutation-types'
-
-export const markLogin = ({dispatch, state}, id) => {
-    dispatch(types.LOGIN, id)
-}
-export const markExit = ({dispatch, state}) => {
-    dispatch(types.EXIT)
-}
--- vue/zone/fe/src/vuex/store.js
+++ vue/zone/fe/src/vuex/store.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import constant from './modules/constant'
-import log from './modules/log'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        constant,
-        log
+        constant
     }
 })

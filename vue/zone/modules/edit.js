25b3db501c2b805d41b510eba9e3d9b01d786be0
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-09-30 16:53:58 +0800

    编辑个人信息 后端逻辑

--- vue/zone/modules/edit.js
+++ vue/zone/modules/edit.js
@@ -4,36 +4,43 @@ var User = require('../models').User;
 var edit = {
     user: function (req, res) {
         var id = req.params.id;
-        if (req.body.portrait) {
-            User.find({id: id}, {portrait: 1}, {}, function (err, result) {
-                var newPortrait = req.body.portrait;
-                var oldPortrait = result[0].portrait;
-
-                if (newPortrait && (oldPortrait != newPortrait)) {
-                    fs.unlink('../upload' + result[0].portrait);
+        User.find({id: id}, {portrait: 1, password: 1}, {}, function (err, result) {
+            if (err) {
+                res.end(JSON.stringify({
+                    message: '网络错误',
+                    status: 0
+                }));
+            } else {
+                if (req.body.portrait) {
+                    var newPortrait = req.body.portrait;
+                    var oldPortrait = result[0].portrait;
+                    if (newPortrait && (oldPortrait != newPortrait)) {
+                        fs.unlink('../upload' + result[0].portrait);
+                    }
                 }
-            });
-        }
-        if (req.body.password == req.body.oldPassword) {
-            User.update({id: id}, req.body, {}, function (err) {
-                if (err) {
-                    res.end(JSON.stringify({
-                        message: '网络错误',
-                        status: 0
-                    }));
-                } else {
-                    res.end(JSON.stringify({
-                        message: '修改成功',
-                        status: 1
-                    }));
+                if (req.body.oldPassword) {
+                    if (result[0].password != req.body.oldPassword) {
+                        res.end(JSON.stringify({
+                            message: '原密码有误',
+                            status: 0
+                        }));
+                    }
                 }
-            });
-        } else {
-            res.end(JSON.stringify({
-                message: '原密码有误',
-                status: 0
-            }));
-        }
+            }
+        });
+        User.update({id: id}, req.body, {}, function (err) {
+            if (err) {
+                res.end(JSON.stringify({
+                    message: '网络错误',
+                    status: 0
+                }));
+            } else {
+                res.end(JSON.stringify({
+                    message: '修改成功',
+                    status: 1
+                }));
+            }
+        });
     }
 };
 

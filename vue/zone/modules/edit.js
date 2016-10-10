65a6af031ed3a83d7d78b5588e5f1e02012f4c01
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-10-10 12:20:40 +0800

    个人页修改个人信息 原密码错误bug

--- vue/zone/modules/edit.js
+++ vue/zone/modules/edit.js
@@ -4,6 +4,21 @@ var User = require('../models').User;
 var edit = {
     user: function (req, res) {
         var id = req.params.id;
+        var update = function () {
+            User.update({id: id}, req.body, {}, function (err) {
+                if (err) {
+                    res.end(JSON.stringify({
+                        message: '网络错误',
+                        status: 0
+                    }));
+                } else {
+                    res.end(JSON.stringify({
+                        message: '修改成功',
+                        status: 1
+                    }));
+                }
+            });
+        };
         User.find({id: id}, {portrait: 1, password: 1}, {}, function (err, result) {
             if (err) {
                 res.end(JSON.stringify({
@@ -24,21 +39,11 @@ var edit = {
                             message: '原密码有误',
                             status: 0
                         }));
+                        return;
                     }
                 }
-            }
-        });
-        User.update({id: id}, req.body, {}, function (err) {
-            if (err) {
-                res.end(JSON.stringify({
-                    message: '网络错误',
-                    status: 0
-                }));
-            } else {
-                res.end(JSON.stringify({
-                    message: '修改成功',
-                    status: 1
-                }));
+
+                update();
             }
         });
     }

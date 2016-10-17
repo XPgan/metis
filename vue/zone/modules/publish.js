6bf284b821fe98552138957aced0712102ac0ee5
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-10-17 17:47:21 +0800

    发布后端逻辑

--- vue/zone/modules/publish.js
+++ vue/zone/modules/publish.js
@@ -1,9 +1,39 @@
+var User = require('../models').User;
 var Article = require('../models').Article;
 
 var publish = function (req, res) {
     var id = (new Date()).valueOf().toString();
-    req.body.id = id;
+    var author = req.body.author;
+    var record = function () {
+          User.find({id: author}, {articles: 1}, {}, function (err, result) {
+            if (err) {
+                res.end(JSON.stringify({
+                    message: '网络错误',
+                    status: 0
+                }));
+            } else {
+                var articles = result[0].articles;
+                articles.push(id);
 
+                User.update({id: author}, {articles: articles}, {}, function (err) {
+                    if (err) {
+                        res.end(JSON.stringify({
+                            message: '网络错误',
+                            status: 0
+                        }));
+                    } else {
+                        res.end(JSON.stringify({
+                            message: '发布成功',
+                            status: 1,
+                            id: id
+                        }));
+                    }
+                });
+            }
+        });
+    };
+
+    req.body.id = id;
     var article = new Article(req.body);
     article.save(function (err) {
         if (err) {
@@ -12,11 +42,7 @@ var publish = function (req, res) {
                 status: 0
             }));
         } else {
-            res.end(JSON.stringify({
-                message: '发布成功',
-                status: 1,
-                id: id
-            }));
+            record();
         }
     });
 }

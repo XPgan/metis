e781f9b7d60b56bb78b7f54f33e2f80d74f28379
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-11-04 10:46:40 +0800

    数据可视化工程文件

---
+++ three/visualization/webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: './entry.js',
+    output: {
+        path: './build',
+        filename: 'bundle.js',
+    },
+    devServer: {
+        port: 8000,
+        inline: true,
+        contentBase: './src'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                loader: 'style!css!less'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/
+            },
+			{
+			    test: /\.(jpg|png|svg)$/,
+                loader: 'url'
+			}
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};

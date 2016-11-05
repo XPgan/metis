80c2307fc61414632b42b1661380fb4e35deb959
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-11-05 17:02:45 +0800

    地图

--- three/visualization/src/main.js
+++ three/visualization/src/main.js
@@ -1,11 +1,13 @@
 import barchart from './modules/barchart'
 import radarmap from './modules/radarmap'
+import chinamap from './modules/chinamap'
 import barchartData from './data/barchart'
 import radarmapData from './data/radarmap'
 
 // 执行
 barchart.sketch(barchartData.opts);
 radarmap.sketch(radarmapData.opts);
+chinamap.sketch({});
 
 // 事件
 barchart.barchart.on('click', function (params) {
---
+++ three/visualization/src/modules/chinamap.js
@@ -0,0 +1,31 @@
+import echarts from 'echarts'
+import '../libs/china'
+
+export default {
+    chinamap: echarts.init(document.getElementById('chinamap')),
+    opts: {
+        tooltip: {
+            trigger: 'item',
+            formatter: '{b}'
+        },
+        series: [
+            {
+                name: '中国',
+                type: 'map',
+                mapType: 'china',
+                selectedMode: 'single',
+                label: {
+                    normal: {
+                        show: true
+                    },
+                    emphasis: {
+                        show: true
+                    }
+                }
+            }
+        ]
+    },
+    sketch (opts) {
+        this.chinamap.setOption(this.opts);
+    }
+}

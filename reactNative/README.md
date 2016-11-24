28439901d9030018dae28b0d664b709aea9bfb62
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-11-24 17:37:46 +0800

    react native 布局

---
+++ reactNative/README.md
@@ -0,0 +1,31 @@
+# React Native #
+
+### #布局 ###
+
++ __flexDirection__ (主轴方向)
+
+    + column
+    
+    + row
+    
++ __justifyContent__ (子元素沿主轴排列方式)
+    
+    + flex-start
+    
+    + flex-end
+    
+    + center
+    
+    + space-around
+    
+    + space-between
+    
++ __alignItems__ (子元素沿次轴排列方式)
+    
+    + flex-start
+    
+    + flex-end
+    
+    + center
+    
+    + stretch
--- reactNative/app/index.android.js
+++ reactNative/app/index.android.js
@@ -8,46 +8,43 @@ import React, {Component} from 'react';
 import {
     AppRegistry,
     StyleSheet,
-    Text,
     View
 } from 'react-native';
 
 export default class app extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.android.js
-                </Text>
-                <Text style={styles.instructions}>
-                    Double tap R on your keyboard to reload,{'\n'}
-                    Shake or press menu button for dev menu
-                </Text>
+            <View style={styles.view}>
+                <View style={styles.first}></View>
+                <View style={styles.second}></View>
+                <View style={styles.third}></View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
+    view: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-end'
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    first: {
+        backgroundColor: 'yellow',
+        width: 90,
+        height: 90
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+    second: {
+        backgroundColor: 'green',
+        width: 100,
+        height: 100
     },
+    third: {
+        backgroundColor: 'blue',
+        width: 50,
+        height: 50
+    }
 });
 
 AppRegistry.registerComponent('app', () => app);

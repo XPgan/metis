6cad95250b8dc46acc3ad685252e24dc049413f3
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-11-25 14:57:32 +0800

    clear

--- reactNative/app/index.android.js
+++ reactNative/app/index.android.js
@@ -8,29 +8,17 @@ import React, {Component} from 'react';
 import {
     AppRegistry,
     StyleSheet,
-    View,
-    ListView,
-    Text
+    View
 } from 'react-native';
 
 export default class app extends Component {
     constructor (props) {
         super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-            dataSource: ds.cloneWithRows([
-                'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
-            ])
-        };
     }
-
     render () {
         return (
             <View>
-                <ListView
-                    dataSource={this.state.dataSource}
-                    renderRow={(rowData) => <Text>{rowData}</Text>}
-                />
+
             </View>
         );
     }

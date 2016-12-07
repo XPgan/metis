330819186368870d31b8f10ea73d7d0c297c5cb6
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-12-07 14:44:16 +0800

    mediaplayer 搭建

--- vue/mediaPlayer/src/main.js
+++ vue/mediaPlayer/src/main.js
@@ -6,6 +6,8 @@ import App from './App'
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
-    template: '<App/>',
-    components: {App}
+    template: '<App />',
+    components: {
+        App
+    }
 })

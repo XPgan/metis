a311804d05c0a07dcf85ccf6d2d52369edea6775
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-12-08 12:18:31 +0800

    全屏按钮箭头

--- vue/mediaPlayer/src/controls/Fullscreen.vue
+++ vue/mediaPlayer/src/controls/Fullscreen.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="fullscreen"></button>
+    <button class="fullscreen trigger"></button>
 </template>
 
 <script>
@@ -16,8 +16,26 @@
         height: 20px;
         margin: 5px;
         float: right;
+        position: relative;
         border-radius: 3px;
         background-color: rgba(255, 255, 255, 0.5);
         box-shadow: 0 0 1px @mainColor;
+
+        &.trigger:before, &.trigger:after {
+            content: "";
+            display: block;
+            position: absolute;
+            width: 0;
+            height: 0;
+            border: 2px solid transparent;
+        }
+        &.trigger:before {
+            border-left-color: @mainColor;
+            border-left-width: 8px;
+        }
+        &.trigger:after {
+            border-right-color: @mainColor;
+            border-right-width: 8px;
+        }
     }
 </style>

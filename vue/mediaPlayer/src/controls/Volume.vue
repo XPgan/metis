a7b2a1c320c616dc88b628344dd929ba45dd8b5b
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-12-08 11:41:12 +0800

    音量减小按钮

--- vue/mediaPlayer/src/controls/Volume.vue
+++ vue/mediaPlayer/src/controls/Volume.vue
@@ -14,6 +14,13 @@
 <style lang="less" scoped>
     @mainColor: #666;
 
+    .center () {
+        top: 50%;
+        left: 50%;
+        -webkit-transform: translate(-50%, -50%);
+                transform: translate(-50%, -50%);
+    }
+
     div.volume {
         float: right;
         margin: 5px;
@@ -22,10 +29,21 @@
             width: 20px;
             height: 20px;
             float: left;
+            position: relative;
             margin: 0 2px;
             border-radius: 3px;
             background-color: rgba(255, 255, 255, 0.5);
             box-shadow: 0 0 1px @mainColor;
+        }
+        .decrease:after {
+            content: "";
+            display: block;
+            position: absolute;
+            width: 6px;
+            height: 2px;
+            background-color: @mainColor;
+
+            .center();
         }
     }
 </style>

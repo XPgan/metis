2f6573b3b9df36283420f6b4ab363fb45b295b68
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-12-16 14:39:23 +0800

    热力图

--- webpack/newsmap/src/main.js
+++ webpack/newsmap/src/main.js
@@ -2,13 +2,16 @@ import barLine from './modules/barLine'
 import funnel from './modules/funnel'
 import radar from './modules/radar'
 import stackedbarLine from './modules/stackedbarLine'
+import heatmap from './modules/heatmap'
 
 import bLData from './data/bLData'
 import fnData from './data/fnData'
 import rdData from './data/rdData'
 import skbLData from './data/skbLData'
+import hmData from './data/hmData'
 
 barLine.sketch(bLData);
 funnel.sketch(fnData);
 radar.sketch(rdData);
 stackedbarLine.sketch(skbLData);
+heatmap.sketch(hmData);

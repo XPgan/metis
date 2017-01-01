d481f4f7d79cd2bc782c9280bdbc8b2d90b3aeb7
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-01-01 17:41:54 +0800

    平行轴图

--- webpack/newsmap/src/main.js
+++ webpack/newsmap/src/main.js
@@ -3,18 +3,18 @@ import funnel from './modules/funnel'
 import radar from './modules/radar'
 import stackedbarLine from './modules/stackedbarLine'
 import heatmap from './modules/heatmap'
-import tree from './modules/tree'
+import parallel from './modules/parallel'
 
 import bLData from './data/bLData'
 import fnData from './data/fnData'
 import rdData from './data/rdData'
 import skbLData from './data/skbLData'
 import hmData from './data/hmData'
-import trData from './data/trData'
+import plData from './data/plData'
 
 barLine.sketch(bLData);
 funnel.sketch(fnData);
 radar.sketch(rdData);
 stackedbarLine.sketch(skbLData);
 heatmap.sketch(hmData);
-tree.sketch(trData);
+parallel.sketch(plData);

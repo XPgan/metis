ae7c56e62970b8c514e3d5e5c06c8d88e369fb1c
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-01-03 14:02:57 +0800

    文件名调整

--- webpack/airpollution/src/main.js
+++ webpack/airpollution/src/main.js
@@ -1,15 +1,11 @@
 import barLine from './modules/barLine'
-import funnel from './modules/funnel'
 import radar from './modules/radar'
-import stackedbarLine from './modules/stackedbarLine'
 import heatmap from './modules/heatmap'
 import parallel from './modules/parallel'
 import cartesian from './modules/cartesian'
 
 import bLData from './data/bLData'
-import fnData from './data/fnData'
 import rdData from './data/rdData'
-import skbLData from './data/skbLData'
 import hmData from './data/hmData'
 import plData from './data/plData'
 import crtData from './data/crtData'
@@ -17,9 +13,7 @@ import crtData from './data/crtData'
 import $ from 'npm-zepto'
 
 barLine.sketch(bLData);
-funnel.sketch(fnData);
 radar.sketch(rdData);
-stackedbarLine.sketch(skbLData);
 heatmap.sketch(hmData);
 parallel.sketch(plData);
 cartesian.sketch(crtData);
--- webpack/newsmap/src/data/fnData.js
+++
@@ -1,9 +0,0 @@
-export default {
-    data: [
-        {value: 60, name: '访问'},
-        {value: 30, name: '咨询'},
-        {value: 10, name: '订单'},
-        {value: 80, name: '点击'},
-        {value: 100, name: '展现'}
-    ]
-}

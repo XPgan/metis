aa055be2fca015e1d9a5abfd5c52fcec06c0bb0f
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-01-03 15:33:52 +0800

    scatter2

--- webpack/airpollution/src/main.js
+++ webpack/airpollution/src/main.js
@@ -4,6 +4,7 @@ import heatmap from './modules/heatmap'
 import parallel from './modules/parallel'
 import cartesian from './modules/cartesian'
 import scatter1 from './modules/scatter1'
+import scatter2 from './modules/scatter2'
 
 import bLData from './data/bLData'
 import rdData from './data/rdData'
@@ -20,6 +21,7 @@ heatmap.sketch(hmData);
 parallel.sketch(plData);
 cartesian.sketch(crtData);
 scatter1.sketch(sctData);
+scatter2.sketch(sctData);
 
 $('.js_crt_menu').on('click', 'li', function (e) {
     var $target = $(e.target);

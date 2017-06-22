71727c11472740ffb31e2b020a8097cdd7bac629
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-06-22 18:59:38 +0800

    tab 逻辑

--- wechat/cocktail/pages/game/game.js
+++ wechat/cocktail/pages/game/game.js
@@ -1,8 +1,14 @@
 Page({
   data: {
-  
+    curTab: 'glass'
   },
   onReady: function () {
   
+  },
+  handleTab: function (e) {
+    var curTab = e.target.dataset.tab;
+    this.setData({
+      curTab: curTab
+    });
   }
 });

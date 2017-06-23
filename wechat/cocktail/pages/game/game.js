847714aa2e50103de659a9828c8cc7f67d5bd979
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-06-23 12:13:59 +0800

    记录参数

--- wechat/cocktail/pages/game/game.js
+++ wechat/cocktail/pages/game/game.js
@@ -6,6 +6,25 @@ Page({
       layer: 'layer1',
       color: '',
       fruit: ''
+    },
+    color: [],
+    // 记录 color 渐变参数
+    layer: {
+      layer1: [0, 1],
+      layer2: [0, 0.67, 1],
+      layer3: [0, 0.5, 0.75, 1],
+      layer4: [0, 0.5, 1],
+      layer5: [0, 0.25, 1],
+      layer6: [0, 0.33, 0.66, 1]
+    },
+    // 记录 fruit 渲染位置
+    fruit: {
+      fruit1: [],
+      fruit2: [],
+      fruit3: [],
+      fruit4: [],
+      fruit5: [],
+      fruit6: []
     }
   },
   onReady: function () {

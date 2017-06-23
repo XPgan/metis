390e3708b367df593fe71e64a58d672054feb2e2
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-06-23 17:32:27 +0800

    cocktail 水果定位

--- wechat/cocktail/pages/game/game.js
+++ wechat/cocktail/pages/game/game.js
@@ -15,6 +15,33 @@ Page({
       layer4: [0, 0.5, 1],
       layer5: [0, 0.25, 1],
       layer6: [0, 0.33, 0.66, 1]
+    },
+    fruit: {
+      fruitPosition: [],
+      glass1: {
+        fruit1: [67, 78],
+        fruit2: [65, 78],
+        fruit3: [65, 85],
+        fruit4: [58, 80],
+        fruit5: [63, 87],
+        fruit6: [60, 75]
+      },
+      glass2: {
+        fruit1: [35, 70],
+        fruit2: [30, 70],
+        fruit3: [30, 75],
+        fruit4: [23, 70],
+        fruit5: [30, 80],
+        fruit6: [25, 70]
+      },
+      glass3: {
+        fruit1: [142, 78],
+        fruit2: [140, 78],
+        fruit3: [140, 84],
+        fruit4: [133, 78],
+        fruit5: [138, 86],
+        fruit6: [135, 72]
+      }
     }
   },
   onReady: function () {
@@ -30,13 +57,21 @@ Page({
     var option = e.target.dataset.option;
     switch (this.data.curTab) {
       case 'glass':
+        var fruitPosition = this.data.fruit[option][this.data.curOption.fruit];
         this.setData({
           'curOption.glass': option
         });
+        this.setData({
+          'fruit.fruitPosition': fruitPosition
+        }); 
         break;
       case 'fruit':
+        var fruitPosition = this.data.fruit[this.data.curOption.glass][option];
         this.setData({
           'curOption.fruit': option
+        }); 
+        this.setData({
+          'fruit.fruitPosition': fruitPosition
         }); 
         break; 
     }

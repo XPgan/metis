13b68922804dec2dec8ff1e7e54cb51f6c79d78b
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-08-28 18:09:24 +0800

    get 请求缓存区分参数

--- node/mock/route.js
+++ node/mock/route.js
@@ -12,7 +12,20 @@ var getRequestCache = {};
 var createGetRequest = function (requestObj) {
     router.get(requestObj.url, function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-        res.end(JSON.stringify(requestObj.data()));
+
+        var paramTmp = '';
+        for (var key in req.query) {
+            paramTmp += '&' + key + '=' + req.query[key];
+        }
+        var paramStr = '?' + paramTmp.slice(1);
+        var requestUrl = requestObj.url + paramStr;
+        if (getRequestCache[requestUrl]) {
+            res.end(JSON.stringify(getRequestCache[requestUrl]));
+        } else {
+            var responseData = requestObj.data();
+            getRequestCache[requestUrl] = responseData;
+            res.end(JSON.stringify(responseData));
+        }
     });
 };
 var createPostRequest = function (requestObj) {

20f65cf5bb62ed8a03d3a31321a58cf9cedd2ce1
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-08-28 18:15:18 +0800

    get 请求参数

--- node/mock/route.js
+++ node/mock/route.js
@@ -11,21 +11,21 @@ var requestGatherLst = [
 var getRequestCache = {};
 var createGetRequest = function (requestObj) {
     router.get(requestObj.url, function (req, res) {
-        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-
         var paramTmp = '';
         for (var key in req.query) {
             paramTmp += '&' + key + '=' + req.query[key];
         }
         var paramStr = '?' + paramTmp.slice(1);
         var requestUrl = requestObj.url + paramStr;
+        var responseData = requestObj.data();
         if (getRequestCache[requestUrl]) {
-            res.end(JSON.stringify(getRequestCache[requestUrl]));
+            responseData = getRequestCache[requestUrl];
         } else {
-            var responseData = requestObj.data();
             getRequestCache[requestUrl] = responseData;
-            res.end(JSON.stringify(responseData));
         }
+
+        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+        res.end(JSON.stringify(responseData));
     });
 };
 var createPostRequest = function (requestObj) {

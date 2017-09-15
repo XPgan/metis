7f29d668c736dfb387b6621dcb27fb82fd3df7fa
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-09-15 14:19:06 +0800

    示例一

--- node/mock/data/example.js
+++ node/mock/data/example.js
@@ -4,35 +4,51 @@ module.exports = {
     example1: {
         url: '/example1/_data',
         type: 'GET',
-        list_name: 'promotions',
-        data: function () {
-            return {
-                message: '请求成功',
-                error: 0,
-                promotions: type.list({
-                    length: 22,
-                    data: function () {
-                        return {
-                            number: type.number()
-                        }
-                    }
-                })
-            }
-        }
-    },
-    example2: {
-        url: '/example2/:id/:id/_data',
-        type: 'GET',
         data: function () {
             return {
                 message: '请求成功',
                 error: 0,
                 data: {
                     id: type.id(),
-                    number: type.number(),
+                    number: type.number({
+                        min: 288,
+                        max: 999
+                    }),
                     bool: type.bool(),
-                    list: type.list({
-                        length: 2
+                    string1: type.string([
+                        '文案一',
+                        '文案二',
+                        '文案三'
+                    ]),
+                    string2: type.string({
+                        minL: 5,
+                        maxL: 16
+                    }),
+                    image1: type.image([
+                        'http://oij8a9ql4.bkt.clouddn.com/default-fe.jpg',
+                        'http://osm0bpix4.bkt.clouddn.com/thumb.jpg'
+                    ]),
+                    image2: type.image({
+                        type: '-thumb'
+                    }),
+                    list1: type.list({
+                        length: 5,
+                        data: function () {
+                            return type.number()
+                        }
+                    }),
+                    list2: type.list({
+                        length: 22,
+                        index: {
+                            name: 'idx',
+                            format: '0\d'
+                        },
+                        data: function () {
+                            return {
+                                pro1: type.number(),
+                                pro2: type.string()
+                            }
+                        }
                     })
                 }
             }

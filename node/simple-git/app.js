54040c1b00b4fc9bd33a89cdf1aa033211e5f548
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-09-25 16:09:16 +0800

    git node

--- node/simple-git/app.js
+++ node/simple-git/app.js
@@ -2,4 +2,21 @@ var express = require('express');
 var app = express();
 var git = require('simple-git');
 
+/* clone */
+git()
+    .clone('git@github.com:sunmengyuan/aphrodite.git');
+
+/* pull */
+git('./aphrodite')
+    .pull('origin', 'master', function (err, update) {
+        console.log(update);
+    });
+
+/* push */
+git('./aphrodite')
+    .add('.')
+    .commit('message')
+    .pull('origin', 'master')
+    .push('origin', 'master');
+
 module.exports = app;

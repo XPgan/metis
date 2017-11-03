92c912816c58d9d185e81ec862c103f76d355356
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-11-03 10:40:24 +0800

    对账单数据

--- node/meowMock/data/ascle.js
+++ node/meowMock/data/ascle.js
@@ -184,5 +184,227 @@ module.exports = {
                 }
             }
         }
+    },
+    statementAll: {
+        url: '/statement/all/_data',
+        type: 'GET',
+        data: function () {
+            return {
+                message: '请求成功',
+                error: 0,
+                data: {
+                    total: 92,
+                    statements: type.list({
+                        length: 10,
+                        data: function () {
+                            return {
+                                id: type.id(),
+                                statement_date: type.number({
+                                    min: 2001,
+                                    max: 2020
+                                }) + '-' + type.number({
+                                    min: 1,
+                                    max: 12
+                                }),
+                                service_order_num: type.number({
+                                    min: 100,
+                                    max: 9999
+                                }),
+                                maidan_order_num: type.number({
+                                    min: 100,
+                                    max: 9999
+                                }),
+                                service_settle_amount: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                }),
+                                maidan_settle_amount: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                }),
+                                total_settle_amount: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                }),
+                                status: type.string()
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    },
+    statementDetail: {
+        url: '/statement/detail/:id',
+        type: 'GET',
+        data: function () {
+            return {
+                message: '请求成功',
+                error: 0,
+                data: {
+                    button_visible: true,
+                    service_settle_id: type.id(),
+                    maidan_settle_id: type.id(),
+                    bank_info: {
+                        account_name: type.string(),
+                        account_number: type.string(),
+                        statement_date: type.string(),
+                        bank: type.string()
+                    },
+                    settle_info: {
+                        maidan_settle_info: {
+                            original_amount: type.number({
+                                min: 1000,
+                                max: 99999
+                            }),
+                            doctor_fee: type.number({
+                                min: 1000,
+                                max: 99999
+                            }),
+                            settle_amount: type.number({
+                                min: 1000,
+                                max: 99999
+                            })
+                        },
+                        service_settle_info: {
+                            normal_service_amount: type.number({
+                                min: 1000,
+                                max: 99999
+                            }),
+                            fenxiao_service_amount: type.number({
+                                min: 1000,
+                                max: 99999
+                            }),
+                            pos_back: type.number({
+                                min: 1000,
+                                max: 99999
+                            }),
+                            settle_amount: type.number({
+                                min: 1000,
+                                max: 99999
+                            }),
+                        },
+                        feededuction_sum: type.number({
+                            min: 1000,
+                            max: 99999
+                        }),
+                        total_settle_amount: type.number({
+                            min: 1000,
+                            max: 99999
+                        })
+                    }
+                }
+            }
+        }
+    },
+    statementHospital: {
+        url: '/statement/hospital/:id/_data',
+        type: 'GET',
+        data: function () {
+            return {
+                message: '请求成功',
+                error: 0,
+                data: {
+                    total: 43,
+                    doctors: type.list({
+                        length: 10,
+                        data: function () {
+                            return {
+                                doctor_id: type.id(),
+                                doctor_name: type.string(),
+                                service_settle_amount: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                }),
+                                maidan_settle_amount: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                }),
+                                total_settle_amount: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                })
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    },
+    statementServiceList: {
+        url: '/statement/service/:id/_data',
+        type: 'GET',
+        data: function () {
+            return {
+                message: '请求成功',
+                error: 0,
+                data: {
+                    total: 43,
+                    orders: type.list({
+                        length: 10,
+                        data: function () {
+                            return {
+                                id: type.id(),
+                                name: type.string(),
+                                item: type.string(),
+                                service_type: type.number({
+                                    min: 0,
+                                    max: 2
+                                }),
+                                service_price: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                }),
+                                pre_payment_price: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                }),
+                                settle_price: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                }),
+                                renmai: type.bool(),
+                                validate_time: type.id()
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    },
+    statementMaidanList: {
+        url: '/statement/maidan/:id/_data',
+        type: 'GET',
+        data: function () {
+            return {
+                message: '请求成功',
+                error: 0,
+                data: {
+                    total: 56,
+                    orders: type.list({
+                        length: 10,
+                        data: function () {
+                            return {
+                                id: type.id(),
+                                name: type.string(),
+                                price: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                }),
+                                payment: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                }),
+                                settle_price: type.number({
+                                    min: 1000,
+                                    max: 99999
+                                }),
+                                pay_time: type.id()
+                            }
+                        }
+                    })
+                }
+            }
+        }
     }
 }

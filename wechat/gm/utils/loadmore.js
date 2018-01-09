8161d7b0c1d0d7e2a4f1e345472d2fe3ea538c83
sunmy <sunmengyuan@wanmeizhensuo.com>
2018-01-09 18:53:33 +0800

    loadmore 更新

--- wechat/gm/utils/loadmore.js
+++ wechat/gm/utils/loadmore.js
@@ -1,26 +1,42 @@
 import Request from 'request'
 
 const Loadmore = {
-    flag: true,
-    page: 1,
-    start_num: 0,
-
-    loadData: (opts = {}) => {
+    loadData: (page, opts = {}) => {
         var callbackFn = opts.callbackFn || function () {};
         var completeFn = opts.completeFn || function () {};
         var nodataFn = opts.nodataFn || function () {};
-        if (Loadmore.flag) {
+        var failFn = opts.failFn || function () {};
+        var errorFn = opts.errorFn || function () {};
+        var _Loadmore = page.data._Loadmore;
+        var status = {
+            page: (_Loadmore && _Loadmore.page) || 1,
+            start_num: (_Loadmore && _Loadmore.start_num) || 0,
+            flag: ((_Loadmore && _Loadmore.flag) != undefined)
+                ? _Loadmore.flag
+                : true
+        };
+        if (status.flag) {
             Request({
                 url: opts.url,
                 params: {
                     ...opts.params,
-                    page: Loadmore.page,
-                    start_num: Loadmore.start_num
+                    page: status.page,
+                    start_num: status.start_num
                 },
                 successFn: (data) => {
                     callbackFn(data.data);
-                    Loadmore.page++;
-                    Loadmore.start_num += 10;
+                    page.setData({
+                        '_Loadmore.page': status.page + 1,
+                        '_Loadmore.start_num': status.start_num + 10
+                    });
+                    if (data.data.length < 10) {
+                        if ((status.page == 1) || (status.start_num == 0)) {
+                            page.setData({
+                                '_Loadmore.flag': false
+                            });
+                            completeFn();
+                        }
+                    }
                 },
                 failFn: (data) => {
                     var isNull = (data.error == 1)
@@ -28,20 +44,28 @@ const Loadmore = {
                         || ((data.data instanceof Array) && !data.data.length)
                         || (JSON.stringify(data.data) == '{}')
                     if (isNull) {
-                        Loadmore.flag = false;
-                        if ((Loadmore.page == 1) || (Loadmore.start_num == 0)) {
+                        page.setData({
+                            '_Loadmore.flag': false
+                        });
+                        if ((status.page == 1) || (status.start_num == 0)) {
                             nodataFn();
                         }
                         completeFn();
                     }
+                    failFn();
+                },
+                errorFn: () => {
+                    errorFn();
                 }
             });
         }
     },
-    clear: () => {
-        Loadmore.flag = true;
-        Loadmore.page = 1;
-        Loadmore.start_num = 0;
+    clear: (page) => {
+        page.setData({
+            '_Loadmore.flag': true,
+            '_Loadmore.page': 1,
+            '_Loadmore.start_num': 0
+        });
     }
 }
 
